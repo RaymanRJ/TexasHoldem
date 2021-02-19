9093f6b4e0c2228046111e721d6f5f2432ea3090
raymanrj <raymanrj@live.com>
2021-02-18 23:46:52 -0500

    Tested threading and multiprocessing - not worth it

--- src/Main.py
+++ src/Main.py
@@ -1,19 +1,52 @@
 from src.Classes.Game import Game
+from multiprocessing import Pool
+import time
 
 # TODO: Review turn order - something's off.
 
-game = Game(num_players=4)
-game.deal(2)
-game.betting_round()    # Pre-flop betting
+num_games = 10000
 
-game.advance_stage()    # Entering FLOP
-game.community_deal()
-game.betting_round()    # Flop betting
+# Multiprocessing:
 
-game.advance_stage()    # Entering RIVER
-game.community_deal()
-game.betting_round()    # River betting
+start_time = time.time()
+games = [Game(num_players=4) for _ in range(num_games)]
 
-game.advance_stage()    # Entering TURN
-game.community_deal()
-game.betting_round()    # Final betting
+
+def thread_function(game: Game):
+    for _ in range(13):
+        game.deal()
+        game.betting_round()
+
+
+with Pool(5) as p:
+    p.map(thread_function, games)
+
+end_time = time.time()
+print(f"Total Multiprocessing time: {end_time - start_time}s.")
+
+# Sequential:
+
+start_time = time.time()
+games = [Game(num_players=4) for _ in range(num_games)]
+
+for game in games:
+    thread_function(game)
+
+end_time = time.time()
+print(f"Total Sequential time: {end_time - start_time}s.")
+
+# game = Game(num_players=4)
+# game.deal(2)
+# game.betting_round()    # Pre-flop betting
+#
+# game.advance_stage()    # Entering FLOP
+# game.community_deal()
+# game.betting_round()    # Flop betting
+#
+# game.advance_stage()    # Entering RIVER
+# game.community_deal()
+# game.betting_round()    # River betting
+#
+# game.advance_stage()    # Entering TURN
+# game.community_deal()
+# game.betting_round()    # Final betting
